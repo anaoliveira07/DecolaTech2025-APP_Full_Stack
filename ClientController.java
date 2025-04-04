61af64132b3a66955cd6d29dff8379991727084e
Ana Letícia <166757729+anaoliveira07@users.noreply.github.com>
2025-04-04 16:19:13 -0300

    Create ClientController.java

---
+++ ClientController.java
@@ -0,0 +1,25 @@
+package com.barbershop.controller;
+
+import com.barbershop.model.Client;
+import com.barbershop.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/clients")
+public class ClientController {
+    @Autowired
+    private ClientService clientService;
+
+    @GetMapping
+    public List<Client> getAllClients() {
+        return clientService.getAllClients();
+    }
+
+    @PostMapping
+    public Client createClient(@RequestBody Client client) {
+        return clientService.createClient(client);
+    }
+}
