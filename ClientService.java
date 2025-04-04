e4b637eba06c4a63c0634f44ddb07540dda4f1fe
Ana Letícia <166757729+anaoliveira07@users.noreply.github.com>
2025-04-04 16:18:41 -0300

    Create ClientService.java

---
+++ ClientService.java
@@ -0,0 +1,22 @@
+package com.barbershop.service;
+
+import com.barbershop.model.Client;
+import com.barbershop.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClientService {
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public List<Client> getAllClients() {
+        return clientRepository.findAll();
+    }
+
+    public Client createClient(Client client) {
+        return clientRepository.save(client);
+    }
+}
